57f2562a90089e36c14a593d9834ba740f57a772
RyuuzinnRaika <ryuuzinn7777777@gmail.com>
2023-08-05 13:51:26 +0900

    kansei

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 # Selenium
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
 driver.get('https://navi.mars.kanazawa-it.ac.jp/portal/student/')
-#driver.quit()
-#start_chrome('https://www.google.co.jp/')
+time.sleep(5)
+driver.quit()
