fe5af743e26492688781cb7e89c5ab6e33eec658
RyuuzinnRaika <ryuuzinn7777777@gmail.com>
2023-08-05 13:01:16 +0900

    send

---
+++ script.py
@@ -0,0 +1,7 @@
+# Selenium
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://navi.mars.kanazawa-it.ac.jp/portal/student/')
+#driver.quit()
+#start_chrome('https://www.google.co.jp/')
